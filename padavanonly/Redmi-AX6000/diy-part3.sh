c53afc109337ea9f8483960c9f5cdea29167bcd6
Leslie-Wong <manyu34@gmail.com>
2023-04-05 02:18:42 +0800

    Update diy-part3.sh

--- padavanonly/Redmi-AX6000/diy-part3.sh
+++ padavanonly/Redmi-AX6000/diy-part3.sh
@@ -4,3 +4,8 @@ cd ../../..
 git pull
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+cd $GITHUB_WORKSPACE/openwrt/feeds/kenzo
+rm -rf luci-app-passwall
+git clone https://github.com/Leslie-Wong/openwrt-passwall -b luci
+mv openwrt-passwall/luci-app-passwall luci-app-passwall
+rm -rf openwrt-passwall
