208ade6809832a91f2c5cf38624f7f6c0b01f93e
Leslie-Wong <manyu34@gmail.com>
2023-04-13 19:19:15 +0800

    Update diy-part2.sh

--- padavanonly/Redmi-AX6000/diy-part2.sh
+++ padavanonly/Redmi-AX6000/diy-part2.sh
@@ -21,9 +21,10 @@ mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 ##---------------------------------------------------------
 
-cd feeds/packages/lang
+cd $GITHUB_WORKSPACE/openwrt/feeds/packages/lang
 rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang
 
-cd openwrt/feeds/luci/applications
+cd $GITHUB_WORKSPACE/openwrt/feeds/luci/applications
 rm -rf luci-app-passwall
 git clone https://github.com/Leslie-Wong/luci-app-passwall.git
+
