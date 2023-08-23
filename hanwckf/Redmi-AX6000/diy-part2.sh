e97c23206194af94913d4428b2619e109061ddd4
Leslie-Wong <manyu34@gmail.com>
2023-08-23 09:52:46 +0800

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -21,5 +21,5 @@ mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 ##---------------------------------------------------------
 
-rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/lang/golang/golang
-svn co https://github.com/openwrt/packages/branches/openwrt-23.05/lang/golang/golang $GITHUB_WORKSPACE/openwrt/feeds/packages/lang/golang/golang
+rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/lang/golang
+svn co https://github.com/openwrt/packages/branches/openwrt-23.05/lang/golang $GITHUB_WORKSPACE/openwrt/feeds/packages/lang/golang
